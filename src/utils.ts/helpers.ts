6bc30aceb136079a861e03c9e0c0eb865387bfbf
Alex Iglesias <servicios@brota.me>
2021-02-27 20:52:04 +0100

    Removed old files

--- src/utils.ts/helpers.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Checks if an element is visible
- * @param {HTMLElement} element
- */
-export const isVisible = (element: HTMLElement) =>
-  !!(element.offsetWidth || element.offsetHeight || element.getClientRects().length);
