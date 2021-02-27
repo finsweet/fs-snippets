5c31df19a716fb2229a6569b2ad18f0fbae0675f
Alex Iglesias <servicios@brota.me>
2021-02-27 21:23:59 +0100

    Fixed HTML escaping characters

--- zendesk/article-page/embed-code.ts
+++ zendesk/article-page/embed-code.ts
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   for (const preElement of preElements) {
     // Get the script source
     const src = preElement.textContent;
-    if (!src) continue;
+    if (!src || !src.startsWith('http')) continue;
 
     // Clear element's content
     preElement.innerHTML = '';
@@ -19,8 +19,11 @@ document.addEventListener('DOMContentLoaded', async () => {
       const response = await fetch(src);
       const code = await response.text();
 
+      // Escape HTML characters
+      const escapedCode = code.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt');
+
       // Insert it in the element
-      preElement.innerHTML = `<code>${code}</code>`;
+      preElement.innerHTML = `<code>${escapedCode}</code>`;
 
       // Highlight the element
       window.hljs.highlightBlock(preElement);
