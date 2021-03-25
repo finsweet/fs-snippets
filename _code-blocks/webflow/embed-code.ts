c6d42e7550d6ed04388b75c31280187cecf30520
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-25 13:39:19 +0100

    Create embed-code.ts

---
+++ _code-blocks/webflow/embed-code.ts
@@ -0,0 +1,41 @@
+import highlightJS from '../hljs';
+import { initCopyClipboard } from '@finsweet/webflow-addons';
+import { createCopyButton, populateCodeElement } from '../helpers';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // Get all the embed elements
+  const embedElements = document.querySelectorAll<HTMLDivElement>('.w-embed');
+
+  for (const [index, embedElement] of embedElements.entries()) {
+    // Get the script source
+    const src = embedElement.textContent;
+    if (!src || !src.startsWith('http')) continue;
+
+    // Clear element's content
+    embedElement.innerHTML = '';
+
+    // Create inner elements
+    const preElement = document.createElement('pre');
+    const codeElement = document.createElement('code');
+    const codeElementId = `hljs-code-${index}`;
+    codeElement.id = codeElementId;
+    preElement.appendChild(codeElement);
+
+    // Populate the code element
+    await populateCodeElement(src, codeElement);
+
+    // Create the copy button
+    const copyButton = createCopyButton(codeElementId);
+
+    // Append the copy button
+    preElement.appendChild(copyButton);
+
+    // Highlight the element
+    highlightJS.highlightBlock(preElement);
+
+    // Append it to the embed
+    embedElement.appendChild(preElement);
+  }
+
+  initCopyClipboard();
+});
