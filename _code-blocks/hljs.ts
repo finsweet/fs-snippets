16eb223142706d43c094a9cb12de5c632e3d144c
Alex Iglesias <alexiglesias@finsweet.com>
2021-03-28 16:15:16 +0200

    Added markdown language detection to hljs

--- _code-blocks/hljs.ts
+++ _code-blocks/hljs.ts
@@ -2,12 +2,14 @@ import hljs from 'highlight.js/lib/core';
 import css from 'highlight.js/lib/languages/css';
 import xml from 'highlight.js/lib/languages/xml';
 import json from 'highlight.js/lib/languages/json';
+import markdown from 'highlight.js/lib/languages/markdown';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('xml', xml);
 hljs.registerLanguage('json', json);
+hljs.registerLanguage('markdown', markdown);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
 
