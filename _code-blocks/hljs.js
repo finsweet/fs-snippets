64d1cdb043bf278367cc16327e7976cf7d5b36e7
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-01-24 14:02:49 +0100

    chore: reformatted with finsweet prettier config

--- _code-blocks/hljs.js
+++ _code-blocks/hljs.js
@@ -1,15 +1,17 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("highlight.js/lib/core"));
-const css_1 = __importDefault(require("highlight.js/lib/languages/css"));
-const xml_1 = __importDefault(require("highlight.js/lib/languages/xml"));
-const json_1 = __importDefault(require("highlight.js/lib/languages/json"));
-const markdown_1 = __importDefault(require("highlight.js/lib/languages/markdown"));
-const javascript_1 = __importDefault(require("highlight.js/lib/languages/javascript"));
-const typescript_1 = __importDefault(require("highlight.js/lib/languages/typescript"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const core_1 = __importDefault(require('highlight.js/lib/core'));
+const css_1 = __importDefault(require('highlight.js/lib/languages/css'));
+const xml_1 = __importDefault(require('highlight.js/lib/languages/xml'));
+const json_1 = __importDefault(require('highlight.js/lib/languages/json'));
+const markdown_1 = __importDefault(require('highlight.js/lib/languages/markdown'));
+const javascript_1 = __importDefault(require('highlight.js/lib/languages/javascript'));
+const typescript_1 = __importDefault(require('highlight.js/lib/languages/typescript'));
 core_1.default.registerLanguage('css', css_1.default);
 core_1.default.registerLanguage('xml', xml_1.default);
 core_1.default.registerLanguage('json', json_1.default);
--- webflow-hacks/cms-hacks/3/3.js
+++ webflow-hacks/cms-hacks/3/3.js
@@ -1,8 +1,7 @@
-"use strict";
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    const jobsCounter = document.querySelectorAll('.cms-jobs-counter').length;
-    const jobsCount = document.querySelector('.hack3-jobs-amount');
-    if (!jobsCount)
-        return;
-    jobsCount.innerHTML = jobsCounter.toString();
+  const jobsCounter = document.querySelectorAll('.cms-jobs-counter').length;
+  const jobsCount = document.querySelector('.hack3-jobs-amount');
+  if (!jobsCount) return;
+  jobsCount.innerHTML = jobsCounter.toString();
 });
