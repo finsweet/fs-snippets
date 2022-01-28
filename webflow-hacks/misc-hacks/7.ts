9cfa42ab7968e40501a91d3066555990ed571cdd
Brian Musuku <brayanfinsweet@github.com>
2022-01-28 06:38:48 +0300

    feat: Hack #7 -Customize favicon for specific page

---
+++ webflow-hacks/misc-hacks/7.ts
@@ -0,0 +1,17 @@
+
+// when the DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) { 
+  /* get a reference to the link element
+  with a rel attribute of icon and store it in the variable link or 
+  if the link does not exist, create a link element
+  and store it in the variable link*/
+  const link = document.querySelector<HTMLAnchorElement>("link[rel*='icon']") || document.createElement('link');
+  // set the link type attribute 
+  link.type = 'image/x-icon';
+  // set the link rel attribute
+  link.rel = 'shortcut icon';
+  // set the link href attribute
+  link.href = 'https://assets.website-files.com/5b8f3bd5334d376d704155c1/5c3610ef8b9a3107c28c79ae_hack7-custom-fav.png';
+  // append the link to the head element
+  document.getElementsByTagName('head')[0].appendChild(link);
+});
