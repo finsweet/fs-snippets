5ffdfda48884e8265cb89b018bd2c1b39d7b9804
Alex Iglesias <alexiglesias@finsweet.com>
2022-01-29 09:39:42 +0100

    Tweaked existing code only the necessary to remove any linting warnings

--- webflow-hacks/form-hacks/1.js
+++ webflow-hacks/form-hacks/1.js
@@ -1,36 +1,36 @@
+'use strict';
 // when the DOM is ready
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the select field.
-    var interestSelectField = document.querySelector('#interest-select-field');
-    // get the email-Form element.
-    var emailForm = document.querySelector('#email-form');
-    // get the success-text element.
-    var successText = document.querySelector('.insert-success-text');
-    if (interestSelectField && emailForm && successText) {
-        // declare & initialize the customSuccessMessage variable 
-        // with the value of the option selected 
-        // on the interest select field
-        var customSuccessMessage_1 = interestSelectField.value;
-        // every time the option on the select field changes
-        interestSelectField.addEventListener('change', function () {
-            // assign the new selected option's value to the customSuccessMessage variable
-            customSuccessMessage_1 = interestSelectField.value;
-        });
-        // when the form's submit button is clicked 
-        emailForm.addEventListener('submit', function (event) {
-            // if the user selected an option on the select field
-            if (customSuccessMessage_1) {
-                // change text of the success-text field 
-                successText.innerText = "Thank you! We'll focus on ".concat(customSuccessMessage_1, " for future F'in sweet Webflow Hacks!");
-                // then submit the form
-                return true;
-            }
-            else { // else if no option was selected
-                // focus on the select field
-                interestSelectField.focus();
-                // stop form submission
-                return false;
-            }
-        });
-    } // end if statement
+document.addEventListener('DOMContentLoaded', () => {
+  // Get the select field.
+  const interestSelectField = document.querySelector('#interest-select-field');
+  // get the email-Form element.
+  const emailForm = document.querySelector('#email-form');
+  // get the success-text element.
+  const successText = document.querySelector('.insert-success-text');
+  if (interestSelectField && emailForm && successText) {
+    // declare & initialize the customSuccessMessage variable
+    // with the value of the option selected
+    // on the interest select field
+    let customSuccessMessage = interestSelectField.value;
+    // every time the option on the select field changes
+    interestSelectField.addEventListener('change', () => {
+      // assign the new selected option's value to the customSuccessMessage variable
+      customSuccessMessage = interestSelectField.value;
+    });
+    // when the form's submit button is clicked
+    emailForm.addEventListener('submit', () => {
+      // if the user selected an option on the select field
+      if (customSuccessMessage) {
+        // change text of the success-text field
+        successText.innerText = `Thank you! We'll focus on ${customSuccessMessage} for future F'in sweet Webflow Hacks!`;
+        // then submit the form
+        return;
+      }
+      // else if no option was selected
+      // focus on the select field
+      interestSelectField.focus();
+      // stop form submission
+      return false;
+    });
+  } // end if statement
 });
