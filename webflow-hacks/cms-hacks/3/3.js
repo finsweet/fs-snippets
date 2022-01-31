70fca7019a2935397d9601298838ccf9377b4c94
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-01-31 11:17:51 +0100

    refactor: added constant declarations to improve updatability and understanding

--- webflow-hacks/cms-hacks/3/3.js
+++ webflow-hacks/cms-hacks/3/3.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // get the amount (length) of elements with data attribute[fs-hacks-element="cms-jobs-counter"]
+  var JOBS_COUNTER_SELECTOR = '[fs-hacks-element="cms-jobs-counter"]';
+  var JOB_COUNT_SELECTOR = '[fs-hacks-element="hack3-job-count"]';
+  // get the amount (length) of elements with selector JOBS_COUNTER_SELECTOR
   // and assign that amount to a constant named jobsCounter
-  var jobsCounter = document.querySelectorAll('[fs-hacks-element="cms-jobs-counter"]').length;
-  // select the element with data attribute [fs-hacks-element="hack3-job-count"]
-  var jobCount = document.querySelector('[fs-hacks-element="hack3-job-count"]');
+  var jobsCounter = document.querySelectorAll(JOBS_COUNTER_SELECTOR).length;
+  // select the element with data attribute JOB_COUNT_SELECTOR
+  var jobCount = document.querySelector(JOB_COUNT_SELECTOR);
   if (!jobCount) return;
   // set the jobsCount textContent to the value of jobsCounter
   jobCount.innerHTML = jobsCounter.toString();
--- webflow-hacks/cms-hacks/3/3.ts
+++ webflow-hacks/cms-hacks/3/3.ts
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // get the amount (length) of elements with data attribute[fs-hacks-element="cms-jobs-counter"]
+  const JOBS_COUNTER_SELECTOR = '[fs-hacks-element="cms-jobs-counter"]';
+  const JOB_COUNT_SELECTOR = '[fs-hacks-element="hack3-job-count"]';
+
+  // get the amount (length) of elements with selector JOBS_COUNTER_SELECTOR
   // and assign that amount to a constant named jobsCounter
-  const jobsCounter = document.querySelectorAll('[fs-hacks-element="cms-jobs-counter"]').length;
+  const jobsCounter = document.querySelectorAll(JOBS_COUNTER_SELECTOR).length;
 
-  // select the element with data attribute [fs-hacks-element="hack3-job-count"]
-  const jobCount = document.querySelector<HTMLElement>('[fs-hacks-element="hack3-job-count"]');
-
+  // select the element with data attribute JOB_COUNT_SELECTOR
+  const jobCount = document.querySelector<HTMLElement>(JOB_COUNT_SELECTOR);
   if (!jobCount) return;
   // set the jobsCount textContent to the value of jobsCounter
   jobCount.innerHTML = jobsCounter.toString();
