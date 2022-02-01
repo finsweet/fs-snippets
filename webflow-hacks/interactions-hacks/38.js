30f1e42b01f16d64689df1c03f1dd494f95fea12
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-02-01 16:23:12 +0100

    chore: revert last commit, un-ran tsc command

--- webflow-hacks/interactions-hacks/38.js
+++ webflow-hacks/interactions-hacks/38.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var FORM_SELECTOR = '[fs-hacks-element="hack38-form"]';
-  var SUCCESS_TRIGGER_SELECTOR = '[fs-hacks-element="hack38-form-success-trigger"]';
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const FORM_SELECTOR = '[fs-hacks-element="hack38-form"]';
+  const SUCCESS_TRIGGER_SELECTOR = '[fs-hacks-element="hack38-form-success-trigger"]';
   // assign form HTML element to variable form
-  var form = document.querySelector(FORM_SELECTOR);
+  const form = document.querySelector(FORM_SELECTOR);
   // assign success trigger HTML element to variable successTrigger
-  var successTrigger = document.querySelector(SUCCESS_TRIGGER_SELECTOR);
+  const successTrigger = document.querySelector(SUCCESS_TRIGGER_SELECTOR);
   if (!form || !successTrigger) return;
   // when form is submitted, execute function triggering submission success
-  var submitEvent = function () {
+  const submitEvent = () => {
     form.onsubmit = triggerSuccess;
   };
   // click our invisible div to that triggers our Webflow Interaction
-  var triggerSuccess = function () {
+  const triggerSuccess = () => {
     successTrigger.click();
   };
   window.onload = submitEvent;
