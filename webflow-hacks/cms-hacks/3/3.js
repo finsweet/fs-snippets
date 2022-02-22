7204183ecd08def3235057423bfc4fbeb81850c9
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-02-22 11:25:21 +0100

    build: update branch with fs-hacks-rewrite latest changes + rebuilt files

--- webflow-hacks/cms-hacks/3/3.js
+++ webflow-hacks/cms-hacks/3/3.js
@@ -1,11 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var JOBS_COUNTER_SELECTOR = '[fs-hacks-element="cms-jobs-counter"]';
-  var JOB_COUNT_SELECTOR = '[fs-hacks-element="hack3-job-count"]';
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const JOBS_COUNTER_SELECTOR = '[fs-hacks-element="cms-jobs-counter"]';
+  const JOB_COUNT_SELECTOR = '[fs-hacks-element="hack3-job-count"]';
   // get the amount (length) of elements with selector JOBS_COUNTER_SELECTOR
   // and assign that amount to a constant named jobsCounter
-  var jobsCounter = document.querySelectorAll(JOBS_COUNTER_SELECTOR).length;
+  const jobsCounter = document.querySelectorAll(JOBS_COUNTER_SELECTOR).length;
   // select the element with data attribute JOB_COUNT_SELECTOR
-  var jobCount = document.querySelector(JOB_COUNT_SELECTOR);
+  const jobCount = document.querySelector(JOB_COUNT_SELECTOR);
   if (!jobCount) return;
   // set the jobsCount textContent to the value of jobsCounter
   jobCount.innerHTML = jobsCounter.toString();
