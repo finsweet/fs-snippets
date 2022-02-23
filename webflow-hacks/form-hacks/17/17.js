e66c6050d330bd6ad45eb1e327a5f04d38a5caf2
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-02-23 11:33:53 +0100

    feat: increment and decrement works after manual input entry + comments

--- webflow-hacks/form-hacks/17/17.js
+++ webflow-hacks/form-hacks/17/17.js
@@ -1,57 +1,54 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+  //assign data attributes to constants
   const FORM_BUTTON_UP_SELECTOR = '[fs-hacks-element="hack17-counter-button-up"]';
   const FORM_BUTTON_DOWN_SELECTOR = '[fs-hacks-element="hack17-counter-button-down"]';
   const COUNTER_INPUT_SELECTOR = '[fs-hacks-element="hack17-counter-input"]';
+  /*
+  In the video, the classes are referencing "Hack 16"
+  This Hack launched as "Hack 17"
+  Please note the change of classes from 16 to 17
+  */
+  // declare & initialize x at 0
   let x = 0;
-  const CounterInput = document.querySelector(COUNTER_INPUT_SELECTOR);
-  if (!CounterInput) return;
+  // select elements with their respective data attributes
+  const counterInput = document.querySelector(COUNTER_INPUT_SELECTOR);
+  if (!counterInput) return;
   const formButtonUp = document.querySelector(FORM_BUTTON_UP_SELECTOR);
   if (!formButtonUp) return;
   const formButtonDown = document.querySelector(FORM_BUTTON_DOWN_SELECTOR);
   if (!formButtonDown) return;
+  // increase counter input value upon clicking on up button
   formButtonUp.addEventListener('click', () => {
+    // convert input value from a number to a string
     const stringValue = JSON.stringify(++x);
-    CounterInput.value = stringValue;
+    // assign that string value to the counter input value
+    counterInput.value = stringValue;
   });
+  // increase counter input value upon clicking on up button
   formButtonDown.addEventListener('click', () => {
+    // only allow decrement if the input value is greater than 0
     if (x > 0) {
+      //convert input value from a number to a string
       const stringValue = JSON.stringify(--x);
-      CounterInput.value = stringValue;
+      // assign that string value to the counter input value
+      counterInput.value = stringValue;
+    }
+  });
+  /* This next part was added post-video recording
+    to make sure the plus and minus work with a manual entry of
+    a number in the text input. If you type 5 into the input manually,
+    and then press (+), it will count to 6 instead of starting to
+    count at 1. Nice!!
+    */
+  // on input value change
+  counterInput.addEventListener('change', () => {
+    // convert input value from a string to a number
+    const num = Number(counterInput.value);
+    // if there's a number
+    if (num) {
+      // assign its value to x
+      x = num;
     }
   });
 });
-// /*
-// In the video, the classes are referencing "Hack 16"
-// This Hack is launched as "Hack 17"
-// Please note the change of classes from 16 to 17
-// */
-// // declare & initialize x at 0
-// // let x = 0;
-// // on button click
-// $('.hack17-counter-button.hack17-up').on('click', () => {
-//   // increment & set new value
-//   $('.hack17-counter-input.w-input').val(++x);
-// });
-// $('.hack17-counter-button.hack17-down').on('click', () => {
-//   // decrement & set new value
-//   if (x > 0) {
-//     $('.hack17-counter-input.w-input').val(--x);
-//   }
-// });
-/* This next part was added post-video recording added
-  to make sure the plus and minus work with a manual entry of
-  a number in the text input. If you type 5 into the input manually,
-  and then press (+), it will count to 6 instead of starting to
-  count at 1. Nice!!
-  */
-// on input value change
-// $('.hack17-counter-input.w-input').change(function () {
-//   // convert input value to number
-//   const num = Number($(this).val());
-//   // if it's a number
-//   if (num) {
-//     // assign its value to x
-//     x = num;
-//   }
-// });
--- webflow-hacks/form-hacks/17/17.ts
+++ webflow-hacks/form-hacks/17/17.ts
@@ -1,23 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //assign data attributes to constants
   const FORM_BUTTON_UP_SELECTOR = '[fs-hacks-element="hack17-counter-button-up"]';
   const FORM_BUTTON_DOWN_SELECTOR = '[fs-hacks-element="hack17-counter-button-down"]';
   const COUNTER_INPUT_SELECTOR = '[fs-hacks-element="hack17-counter-input"]';
 
+  /*
+In the video, the classes are referencing "Hack 16"
+This Hack launched as "Hack 17"
+Please note the change of classes from 16 to 17
+*/
+
+  // declare & initialize x at 0
   let x = 0;
-  const CounterInput = document.querySelector<HTMLSelectElement>(COUNTER_INPUT_SELECTOR);
-  if (!CounterInput) return;
+
+  // select elements with their respective data attributes
+  const counterInput = document.querySelector<HTMLSelectElement>(COUNTER_INPUT_SELECTOR);
+  if (!counterInput) return;
+
   const formButtonUp = document.querySelector<HTMLSelectElement>(FORM_BUTTON_UP_SELECTOR);
   if (!formButtonUp) return;
+
   const formButtonDown = document.querySelector<HTMLSelectElement>(FORM_BUTTON_DOWN_SELECTOR);
   if (!formButtonDown) return;
+
+  // increase counter input value upon clicking on up button
   formButtonUp.addEventListener('click', () => {
+    // convert input value from a number to a string
     const stringValue = JSON.stringify(++x);
-    CounterInput.value = stringValue;
+    // assign that string value to the counter input value
+    counterInput.value = stringValue;
   });
+  // increase counter input value upon clicking on up button
+
   formButtonDown.addEventListener('click', () => {
+    // only allow decrement if the input value is greater than 0
     if (x > 0) {
+      //convert input value from a number to a string
       const stringValue = JSON.stringify(--x);
-      CounterInput.value = stringValue;
+      // assign that string value to the counter input value
+      counterInput.value = stringValue;
+    }
+  });
+
+  /* This next part was added post-video recording 
+  to make sure the plus and minus work with a manual entry of
+  a number in the text input. If you type 5 into the input manually,
+  and then press (+), it will count to 6 instead of starting to
+  count at 1. Nice!!
+  */
+
+  // on input value change
+  counterInput.addEventListener('change', () => {
+    // convert input value from a string to a number
+    const num = Number(counterInput.value);
+    // if there's a number
+    if (num) {
+      // assign its value to x
+      x = num;
     }
   });
 });
