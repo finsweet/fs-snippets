cf4c39382277af8d6b039bb5e34866d0d2e6b926
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2022-02-25 14:43:50 +0100

    feat: ability to use multiple independendent counters on one page + refactored function

--- webflow-hacks/form-hacks/17/17.js
+++ webflow-hacks/form-hacks/17/17.js
@@ -9,46 +9,34 @@ document.addEventListener('DOMContentLoaded', () => {
   This Hack launched as "Hack 17"
   Please note the change of classes from 16 to 17
   */
-  // declare & initialize x at 0
-  let x = 0;
   // select elements with their respective data attributes
-  const counterInput = document.querySelector(COUNTER_INPUT_SELECTOR);
-  if (!counterInput) return;
-  const formButtonUp = document.querySelector(FORM_BUTTON_UP_SELECTOR);
-  if (!formButtonUp) return;
-  const formButtonDown = document.querySelector(FORM_BUTTON_DOWN_SELECTOR);
-  if (!formButtonDown) return;
-  // increase counter input value upon clicking on up button
-  formButtonUp.addEventListener('click', () => {
-    // convert input value from a number to a string and assign it to constant stringValue
-    const stringValue = (++x).toString();
-    // assign that string value to the counter input value
-    counterInput.value = stringValue;
-  });
-  // increase counter input value upon clicking on up button
-  formButtonDown.addEventListener('click', () => {
-    // only allow decrement if the input value is greater than 0
-    if (x > 0) {
-      //convert input value from a number to a string and assign it to constant stringValue
-      const stringValue = (--x).toString();
-      // assign that string value to the counter input value
+  const counterInputAll = document.querySelectorAll(COUNTER_INPUT_SELECTOR);
+  if (!counterInputAll) return;
+  const formButtonUpAll = document.querySelectorAll(FORM_BUTTON_UP_SELECTOR);
+  if (!formButtonUpAll) return;
+  const formButtonDownAll = document.querySelectorAll(FORM_BUTTON_DOWN_SELECTOR);
+  if (!formButtonDownAll) return;
+  // loop through each counter input
+  counterInputAll.forEach((item, index) => {
+    // declare individual components
+    const counterInput = counterInputAll[index];
+    const formButtonUp = formButtonUpAll[index];
+    const formButtonDown = formButtonDownAll[index];
+    // declare & initialize x at 0
+    let x = 0;
+    // function assigning the counter value
+    const setValue = (num) => {
+      const stringValue = num.toString();
       counterInput.value = stringValue;
-    }
-  });
-  /* This next part was added post-video recording
-    to make sure the plus and minus work with a manual entry of
-    a number in the text input. If you type 5 into the input manually,
-    and then press (+), it will count to 6 instead of starting to
-    count at 1. Nice!!
-    */
-  // on input value change
-  counterInput.addEventListener('change', () => {
-    // convert input value from a string to a number and assign it to constant num
-    const num = Number(counterInput.value);
-    // if num exists
-    if (num) {
-      // assign its value to x
-      x = num;
-    }
+    };
+    // increase counter input value upon clicking on button up
+    formButtonUp.addEventListener('click', () => {
+      setValue(++x);
+    });
+    // decrease counter input value upon clicking on button down
+    formButtonDown.addEventListener('click', () => {
+      if (x <= 0) return;
+      setValue(--x);
+    });
   });
 });
