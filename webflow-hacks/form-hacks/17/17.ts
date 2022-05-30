de8d360b92225c169e281776a427f5898639282b
Brian Musuku <brayanfinsweet@github.com>
2022-05-30 11:25:38 +0300

    fix: refactor  to account for multiple counters

--- webflow-hacks/form-hacks/17/17.ts
+++ webflow-hacks/form-hacks/17/17.ts
@@ -3,28 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const FORM_BUTTON_UP_SELECTOR = '[fs-hacks-element="hack17-counter-button-up"]';
   const FORM_BUTTON_DOWN_SELECTOR = '[fs-hacks-element="hack17-counter-button-down"]';
   const COUNTER_INPUT_SELECTOR = '[fs-hacks-element="hack17-counter-input"]';
-
-  /*
-In the video, the classes are referencing "Hack 16"
-This Hack launched as "Hack 17"
-Please note the change of classes from 16 to 17
-*/
+  const COUNTER_DIV_SELECTOR = '[fs-hacks-element="hack17-counter-div"]';
 
   // select elements with their respective data attributes
-  const counterInputs = document.querySelectorAll<HTMLSelectElement>(COUNTER_INPUT_SELECTOR);
-  if (!counterInputs) return;
-
-  const incrementButtons = document.querySelectorAll<HTMLSelectElement>(FORM_BUTTON_UP_SELECTOR);
-  if (!incrementButtons) return;
-
-  const decrementButtons = document.querySelectorAll<HTMLSelectElement>(FORM_BUTTON_DOWN_SELECTOR);
-  if (!decrementButtons) return;
+  const counterDivs = document.querySelectorAll<HTMLDivElement>(COUNTER_DIV_SELECTOR);
+  if (!counterDivs) return;
 
   // loop through each counter input
-  counterInputs.forEach((counterInput, index) => {
+  counterDivs.forEach((counterDiv) => {
     // declare individual increment and decrement buttons
-    const incrementButton = incrementButtons[index];
-    const decrementButton = decrementButtons[index];
+    const incrementButton = counterDiv.querySelector<HTMLButtonElement>(FORM_BUTTON_UP_SELECTOR);
+    const decrementButton = counterDiv.querySelector<HTMLButtonElement>(FORM_BUTTON_DOWN_SELECTOR);
+    const counterInput = counterDiv.querySelector<HTMLInputElement>(COUNTER_INPUT_SELECTOR);
+    if (!incrementButton || !decrementButton || !counterInput) return;
 
     // declare & initialize x at 0
     let x = 0;
@@ -37,13 +28,13 @@ Please note the change of classes from 16 to 17
 
     // increase counter input value upon clicking on button up
     incrementButton.addEventListener('click', () => {
-      setValue(++x);
+      setValue((x += 1));
     });
 
     // decrease counter input value upon clicking on button down
     decrementButton.addEventListener('click', () => {
       if (x <= 0) return;
-      setValue(--x);
+      setValue((x -= 1));
     });
   });
 });
