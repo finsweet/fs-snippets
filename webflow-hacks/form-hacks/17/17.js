b25f10881f5b26a21c1b9df3dee9c855387c97b6
Brian Musuku <brayanfinsweet@github.com>
2022-05-30 11:58:42 +0300

    build: run 17.ts

--- webflow-hacks/form-hacks/17/17.js
+++ webflow-hacks/form-hacks/17/17.js
@@ -1,40 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
   //assign data attributes to constants
-  var FORM_BUTTON_UP_SELECTOR = '[fs-hacks-element="hack17-counter-button-up"]';
-  var FORM_BUTTON_DOWN_SELECTOR = '[fs-hacks-element="hack17-counter-button-down"]';
-  var COUNTER_INPUT_SELECTOR = '[fs-hacks-element="hack17-counter-input"]';
-  /*
-  In the video, the classes are referencing "Hack 16"
-  This Hack launched as "Hack 17"
-  Please note the change of classes from 16 to 17
-  */
+  const FORM_BUTTON_UP_SELECTOR = '[fs-hacks-element="hack17-counter-button-up"]';
+  const FORM_BUTTON_DOWN_SELECTOR = '[fs-hacks-element="hack17-counter-button-down"]';
+  const COUNTER_INPUT_SELECTOR = '[fs-hacks-element="hack17-counter-input"]';
+  const COUNTER_DIV_SELECTOR = '[fs-hacks-element="hack17-counter-div"]';
   // select elements with their respective data attributes
-  var counterInputs = document.querySelectorAll(COUNTER_INPUT_SELECTOR);
-  if (!counterInputs) return;
-  var incrementButtons = document.querySelectorAll(FORM_BUTTON_UP_SELECTOR);
-  if (!incrementButtons) return;
-  var decrementButtons = document.querySelectorAll(FORM_BUTTON_DOWN_SELECTOR);
-  if (!decrementButtons) return;
+  const counterDivs = document.querySelectorAll(COUNTER_DIV_SELECTOR);
+  if (!counterDivs) return;
   // loop through each counter input
-  counterInputs.forEach(function (counterInput, index) {
+  counterDivs.forEach((counterDiv) => {
     // declare individual increment and decrement buttons
-    var incrementButton = incrementButtons[index];
-    var decrementButton = decrementButtons[index];
+    const incrementButton = counterDiv.querySelector(FORM_BUTTON_UP_SELECTOR);
+    const decrementButton = counterDiv.querySelector(FORM_BUTTON_DOWN_SELECTOR);
+    const counterInput = counterDiv.querySelector(COUNTER_INPUT_SELECTOR);
+    if (!incrementButton || !decrementButton || !counterInput) return;
     // declare & initialize x at 0
-    var x = 0;
+    let x = 0;
     // function assigning the counter value
-    var setValue = function (num) {
-      var stringValue = num.toString();
+    const setValue = (num) => {
+      const stringValue = num.toString();
       counterInput.value = stringValue;
     };
     // increase counter input value upon clicking on button up
-    incrementButton.addEventListener('click', function () {
-      setValue(++x);
+    incrementButton.addEventListener('click', () => {
+      setValue((x += 1));
     });
     // decrease counter input value upon clicking on button down
-    decrementButton.addEventListener('click', function () {
+    decrementButton.addEventListener('click', () => {
       if (x <= 0) return;
-      setValue(--x);
+      setValue((x -= 1));
     });
   });
 });
