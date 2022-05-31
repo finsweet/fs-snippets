859b27f34f34c4cdd9ec842d83aa04a6e924511a
Brian Musuku <brayanfinsweet@github.com>
2022-05-31 08:47:03 +0300

    feat: Run a function on button click

---
+++ webflow-hacks/misc-hacks/12/12.ts
@@ -0,0 +1,17 @@
+// when DOM ready
+document.addEventListener('DOMContentLoaded', function () {
+  // selector constants
+  const BUTTON_SELECTOR = '[fs-hacks-element="hack-button"]';
+  const TEXT_SELECTOR = '[fs-hacks-element="hack-text"]';
+  // get button and text elements
+  const button = document.querySelector(BUTTON_SELECTOR);
+  const text = document.querySelector(TEXT_SELECTOR);
+  // early return if no button
+  if (!button || !text) return;
+  // on button click listener
+  button.addEventListener('click', () => {
+    // function - start
+    text.innerHTML = 'This is me on Webflow.';
+    // function - end
+  });
+});
