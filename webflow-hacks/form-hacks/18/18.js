602bd4c50add7748d2329d8cef8b4c8d68b0df3f
Brian Musuku <brayanfinsweet@github.com>
2022-06-06 13:53:03 +0300

    Hack 18 : Reject gmail & other non-business emails from form submission

---
+++ webflow-hacks/form-hacks/18/18.js
@@ -0,0 +1,28 @@
+'use strict';
+// when the DOM is ready
+document.addEventListener('DOMContentLoaded', function () {
+  // make an array of invalid domains
+  const invalidDomains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'competitor.com'];
+  // set the fs-hacks selector
+  const SUBMIT_BUTTON_SELECTOR = '[fs-hacks-element="submit"]';
+  const EMAIL_SELECTOR = '[fs-hacks-element="email"]';
+  // get the dom elements
+  const submitBtn = document.querySelector(SUBMIT_BUTTON_SELECTOR);
+  const emailInput = document.querySelector(EMAIL_SELECTOR);
+  // check if the elements exists
+  if (!submitBtn || !emailInput) return;
+  // on submit button click
+  submitBtn.addEventListener('click', function () {
+    // split email at '@' character to get domain
+    const domainPart = emailInput.value.split('@')[1];
+    // if the domain exists in the invalidDomains array
+    if (invalidDomains.indexOf(domainPart) !== -1) {
+      // clear email field
+      emailInput.value = '';
+      // add a 'use business mail' placeholder
+      emailInput.setAttribute('placeholder', 'Please enter a business email');
+      // prevent form submission
+      return false;
+    }
+  });
+});
