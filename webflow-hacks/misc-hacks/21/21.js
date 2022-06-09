8917c2abf22ed158067846cf026f68bf1a44ed74
Brian Musuku <brayanfinsweet@github.com>
2022-06-09 13:49:32 +0300

    feat: Create an age gate to confirm the user's age

---
+++ webflow-hacks/misc-hacks/21/21.js
@@ -0,0 +1,10 @@
+'use strict';
+/* put all of this code in <head> of Site Settings if you want to
+gate every page on your site until the age gate is filled out! */
+// if no validAge cookie *AND* you are not on the Age Gate page (update post video-record)
+// this *AND* is needed if you add this redirect code to site settings. Without the AND you will be caught in an infinite redirect loop!
+const AGE_GATE_URL = 'https://finsweethacks.com/success/21-age-gate';
+if (document.cookie.indexOf('validAge') === -1 && window.location.href !== AGE_GATE_URL) {
+  // redirect to /age-gate page
+  window.location.replace(AGE_GATE_URL);
+}
---
+++ webflow-hacks/misc-hacks/21/gate.js
@@ -0,0 +1,65 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const FORM_SELECTOR = '[fs-hacks-element="age-gate"]';
+  const YEAR_SELECTOR = '[fs-hacks-element="year"]';
+  const MONTH_SELECTOR = '[fs-hacks-element="month"]';
+  const DAY_SELECTOR = '[fs-hacks-element="day"]';
+  const ERROR_DIV_SELECTOR = '[fs-hacks-element="error"]';
+  const form = document.querySelector(FORM_SELECTOR);
+  const yearElement = document.querySelector(YEAR_SELECTOR);
+  const monthElement = document.querySelector(MONTH_SELECTOR);
+  const dayElement = document.querySelector(DAY_SELECTOR);
+  const errorDiv = document.querySelector(ERROR_DIV_SELECTOR);
+  const PROTECTED_PAGE = 'https://finsweethacks.com/21';
+  if (!form || !yearElement || !monthElement || !dayElement || !errorDiv) return;
+  // on form submit.
+  form.addEventListener('submit', () => {
+    // get year, month & day values
+    const year = yearElement.value;
+    const month = monthElement.value;
+    const day = dayElement.value;
+    // make date string
+    // e.g. '2000-09-02'
+    const date = year + '/' + month + '/' + day;
+    // if user's age >= 21
+    if (getAge(date) >= 21) {
+      // give the user a validAge cookie
+      document.cookie = 'validAge=true; max-age=31536000';
+      // redirect to hack page
+      window.location.replace(PROTECTED_PAGE);
+    } else {
+      // if user's age < 21
+      // show error message
+      errorDiv.style.display = 'block';
+      // remove error message after 2 sec
+      setTimeout(() => {
+        errorDiv.style.display = 'none';
+      }, 2000);
+    }
+  });
+});
+/**
+ * This function returns the age of the user in years. It takes a date string as an argument.
+ * @param {string} dateString - A date string in the format 'YYYY-MM-DD'.
+ * @returns {number} - The age of the user in years.
+ * */
+function getAge(dateString) {
+  // get today's date
+  const today = new Date();
+  // get user birth date
+  const birthDate = new Date(dateString);
+  // calculate user age
+  // subtract their birth year from current year
+  let age = today.getFullYear() - birthDate.getFullYear();
+  // // subtract user birth month from current month
+  const month = today.getMonth() - birthDate.getMonth();
+  // if month <= 0
+  // OR
+  // today's date is less than user's birth date
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    // reduce user's age by 1
+    age -= 1;
+  }
+  // return user's current age
+  return age;
+}
