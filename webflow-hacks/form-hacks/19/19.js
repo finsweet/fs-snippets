cf55c450e81f63a8281312bf6926fa48bfd610b1
Brian Musuku <brayanfinsweet@github.com>
2022-06-09 08:42:15 +0300

    fix: remove redudant code

--- webflow-hacks/form-hacks/19/19.js
+++ webflow-hacks/form-hacks/19/19.js
@@ -8,20 +8,14 @@ if you don't have it entered correctly
 */
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
-  // create variables for our form elements in designer
-  // get the select field
+  // declare constants
   const FORM_SELECTOR = '[fs-hacks-element="form"]';
-  const SELECT_INPUT_SELECTOR = '[fs-hacks-element="hack-form-select"]';
-  const RADIO_SELECTOR = '[fs-hacks-element="hack19-radio"]';
   const CHECKBOX_SELECTOR = '[fs-hacks-element="hack19-checkbox"]';
   const HIDDEN_INTEREST_SELECTOR = 'input[name="INTEREST"]';
   const HIDDEN_EXPERIENCE_SELECTOR = 'input[name="EXP"]';
   const HIDDEN_DESIGNER_SELECTOR = 'input[name="CHECKDES"]';
   const HIDDEN_DEVELOPER_SELECTOR = 'input[name="CHECKDEv"]';
   const HIDDEN_PM_SELECTOR = 'input[name="CHECKPM"]';
-  const formSelect = document.querySelector(SELECT_INPUT_SELECTOR);
-  // get the radio button fields
-  const formRadios = document.querySelectorAll(RADIO_SELECTOR);
   // get the checkbox fields
   const formCheckboxes = document.querySelectorAll(CHECKBOX_SELECTOR);
   // create variables for our hidden text fields that will be submitted
@@ -38,35 +32,11 @@ document.addEventListener('DOMContentLoaded', function () {
   // form element
   const form = document.querySelector(FORM_SELECTOR);
   // early return
-  if (
-    !form ||
-    !checkpmInp ||
-    !checkdesInp ||
-    !checkdevInp ||
-    !interestInp ||
-    !expInp ||
-    !formCheckboxes ||
-    !formSelect ||
-    !formRadios
-  )
-    return;
+  if (!form || !checkpmInp || !checkdesInp || !checkdevInp || !interestInp || !expInp || !formCheckboxes) return;
   // on submit form
   form.addEventListener('submit', function (e) {
     // prevent default behavior
     e.preventDefault();
-    // get the form-select's selected value
-    const selectVal = formSelect.value;
-    // get the checked radio button's value
-    const radioArray = Array.from(formRadios);
-    const checkedRadio = radioArray.find((radio) => radio.checked);
-    if (!checkedRadio) return;
-    const radioVal = checkedRadio.value;
-    // SELECT FIELD
-    // set the interest hidden input field's value to selectVal
-    interestInp.value = selectVal;
-    // RADIO
-    // set the experience hidden input field's value to radioVal
-    expInp.value = radioVal;
     // for each checkbox
     formCheckboxes.forEach(function (checkbox) {
       // check if it's checked (true) or not (false)
