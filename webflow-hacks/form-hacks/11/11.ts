e6c591bb57b158ace9bbdc86378581ad20e81978
Brian Musuku <brayanfinsweet@github.com>
2022-06-13 12:54:14 +0300

    refactor: change all vars to camel case

--- webflow-hacks/form-hacks/11/11.ts
+++ webflow-hacks/form-hacks/11/11.ts
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   const SHOW_PAGE_URL_SELECTOR = '[fs-hacks-element="show-page-url"]';
   const PAGE_URL_INPUT_SELECTOR = '[fs-hacks-element="page-url-input"]';
   // get elements
-  const page_url = document.querySelector<HTMLDivElement>(SHOW_PAGE_URL_SELECTOR);
-  const page_url_input = document.querySelector<HTMLInputElement>(PAGE_URL_INPUT_SELECTOR);
+  const pageUrl = document.querySelector<HTMLDivElement>(SHOW_PAGE_URL_SELECTOR);
+  const pageUrlInput = document.querySelector<HTMLInputElement>(PAGE_URL_INPUT_SELECTOR);
   // early return if the element is not found
-  if (!page_url || !page_url_input) return;
+  if (!pageUrl || !pageUrlInput) return;
   // get & store the page url in a variable
   const url = location.href;
   // set the page url as the hidden input's value
-  page_url_input.value = url;
+  pageUrlInput.value = url;
   // show the page url as text on the page
   // this last line is only used as a visual for our Hack!
-  page_url.innerText = url;
+  pageUrl.innerText = url;
 });
