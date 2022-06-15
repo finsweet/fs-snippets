c766c4a3dc43c9afb3f784c0bde76e83819e1d82
Brian Musuku <brayanfinsweet@github.com>
2022-06-15 14:22:39 +0300

    refactor: remove attr & add name input in form scope

--- webflow-hacks/form-hacks/29/29.js
+++ webflow-hacks/form-hacks/29/29.js
@@ -4,14 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   // declare constant selectors
   const FORM_SELECTOR = '[fs-hacks-element="hack29-form"]';
   const NAME_INPUT_SELECTOR = '[fs-hacks-element="hack29-name-input"]';
-  const MESSAGE_SELECTOR = '[fs-hacks-element="hack29-custom-message"]';
-  const nameInput = document.querySelector(NAME_INPUT_SELECTOR);
+  const MESSAGE_SELECTOR = '.w-form-done';
   const messageDiv = document.querySelector(MESSAGE_SELECTOR);
   const form = document.querySelector(FORM_SELECTOR);
   // early return
-  if (!form || !nameInput || !messageDiv) return;
+  if (!form || !messageDiv) return;
   // when form is submitted
   form.addEventListener('submit', function () {
+    const nameInput = this.querySelector(NAME_INPUT_SELECTOR);
+    if (!nameInput) return;
     const nameValue = nameInput.value;
     if (nameValue && nameValue !== '') {
       messageDiv.innerText = `Thank you, ${nameValue}!`;
--- webflow-hacks/form-hacks/29/29.ts
+++ webflow-hacks/form-hacks/29/29.ts
@@ -3,14 +3,15 @@ document.addEventListener('DOMContentLoaded', function () {
   // declare constant selectors
   const FORM_SELECTOR = '[fs-hacks-element="hack29-form"]';
   const NAME_INPUT_SELECTOR = '[fs-hacks-element="hack29-name-input"]';
-  const MESSAGE_SELECTOR = '[fs-hacks-element="hack29-custom-message"]';
-  const nameInput = document.querySelector<HTMLInputElement>(NAME_INPUT_SELECTOR);
+  const MESSAGE_SELECTOR = '.w-form-done';
   const messageDiv = document.querySelector<HTMLDivElement>(MESSAGE_SELECTOR);
   const form = document.querySelector<HTMLFormElement>(FORM_SELECTOR);
   // early return
-  if (!form || !nameInput || !messageDiv) return;
+  if (!form || !messageDiv) return;
   // when form is submitted
   form.addEventListener('submit', function () {
+    const nameInput = this.querySelector<HTMLInputElement>(NAME_INPUT_SELECTOR);
+    if (!nameInput) return;
     const nameValue = nameInput.value;
     if (nameValue && nameValue !== '') {
       messageDiv.innerText = `Thank you, ${nameValue}!`;
