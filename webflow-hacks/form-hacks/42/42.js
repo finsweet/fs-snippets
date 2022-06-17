14a2bebed90747b1dfa72a49f24adfd3c2362d46
Brian Musuku <brayanfinsweet@github.com>
2022-06-17 19:05:25 +0300

    refactor: more correct code

--- webflow-hacks/form-hacks/42/42.js
+++ webflow-hacks/form-hacks/42/42.js
@@ -1,23 +1,26 @@
 'use strict';
 // when dom is ready
 document.addEventListener('DOMContentLoaded', function () {
-  const CHECKBOX_SELECTOR = '[finsweet-hacks-element="checkbox"]';
-  const TOTAL_SELECTOR = '[finsweet-hacks-element="total-value"]';
-  const HIDDEN_INPUT_SELECTOR = '[finsweet-hacks-element="hidden-total"]';
-  const checkboxDivs = document.querySelectorAll(CHECKBOX_SELECTOR);
+  const CHECKBOX_SELECTOR = '[fs-hacks-element="checkbox"]';
+  const TOTAL_SELECTOR = '[fs-hacks-element="total-value"]';
+  const HIDDEN_INPUT_SELECTOR = '[fs-hacks-element="hidden-total"]';
+  const checkboxs = document.querySelectorAll(CHECKBOX_SELECTOR);
   const totalValueDiv = document.querySelector(TOTAL_SELECTOR);
   const hiddenTotalInput = document.querySelector(HIDDEN_INPUT_SELECTOR);
-  if (checkboxDivs.length === 0 || !totalValueDiv || !hiddenTotalInput) return;
+  if (checkboxs.length === 0 || !totalValueDiv || !hiddenTotalInput) return;
   let sum = 0;
-  // on each checkbox click
-  checkboxDivs.forEach((checkboxDiv) => {
-    checkboxDiv.addEventListener('click', function () {
-      // get the checkbox input field
-      const checkbox = checkboxDiv.previousElementSibling;
-      if (!checkbox) return;
+  // on each checkbox input change
+  checkboxs.forEach((checkbox) => {
+    checkbox.addEventListener('input', function () {
       const amountToBeAdded = Number(checkbox.getAttribute('add-value'));
+      // check if amountToBeAdded is Nan
+      if (isNaN(amountToBeAdded)) return;
       // add or subtract amountToBeAdded given the checkbox state
-      checkbox.checked ? (sum -= amountToBeAdded) : (sum += amountToBeAdded);
+      if (!this.checked) {
+        sum -= amountToBeAdded;
+      } else {
+        sum += amountToBeAdded;
+      }
       // format sum e.g. 3500 to 3,500
       const formattedSum = new Intl.NumberFormat().format(sum);
       totalValueDiv.innerText = formattedSum;
--- webflow-hacks/form-hacks/42/42.ts
+++ webflow-hacks/form-hacks/42/42.ts
@@ -1,22 +1,25 @@
 // when dom is ready
 document.addEventListener('DOMContentLoaded', function () {
-  const CHECKBOX_SELECTOR = '[finsweet-hacks-element="checkbox"]';
-  const TOTAL_SELECTOR = '[finsweet-hacks-element="total-value"]';
-  const HIDDEN_INPUT_SELECTOR = '[finsweet-hacks-element="hidden-total"]';
-  const checkboxDivs = document.querySelectorAll(CHECKBOX_SELECTOR);
+  const CHECKBOX_SELECTOR = '[fs-hacks-element="checkbox"]';
+  const TOTAL_SELECTOR = '[fs-hacks-element="total-value"]';
+  const HIDDEN_INPUT_SELECTOR = '[fs-hacks-element="hidden-total"]';
+  const checkboxs = document.querySelectorAll<HTMLInputElement>(CHECKBOX_SELECTOR);
   const totalValueDiv = document.querySelector<HTMLDivElement>(TOTAL_SELECTOR);
   const hiddenTotalInput = document.querySelector<HTMLInputElement>(HIDDEN_INPUT_SELECTOR);
-  if (checkboxDivs.length === 0 || !totalValueDiv || !hiddenTotalInput) return;
+  if (checkboxs.length === 0 || !totalValueDiv || !hiddenTotalInput) return;
   let sum = 0;
-  // on each checkbox click
-  checkboxDivs.forEach((checkboxDiv) => {
-    checkboxDiv.addEventListener('click', function () {
-      // get the checkbox input field
-      const checkbox = checkboxDiv.previousElementSibling as HTMLInputElement;
-      if (!checkbox) return;
+  // on each checkbox input change
+  checkboxs.forEach((checkbox) => {
+    checkbox.addEventListener('input', function () {
       const amountToBeAdded = Number(checkbox.getAttribute('add-value'));
+      // check if amountToBeAdded is Nan
+      if (isNaN(amountToBeAdded)) return;
       // add or subtract amountToBeAdded given the checkbox state
-      checkbox.checked ? (sum -= amountToBeAdded) : (sum += amountToBeAdded);
+      if (!this.checked) {
+        sum -= amountToBeAdded;
+      } else {
+        sum += amountToBeAdded;
+      }
       // format sum e.g. 3500 to 3,500
       const formattedSum = new Intl.NumberFormat().format(sum);
       totalValueDiv.innerText = formattedSum;
