b76076ff534e22f3518e2421b0b03da88c4e6e41
Brian Musuku <brayanfinsweet@github.com>
2022-06-17 14:25:06 +0300

    Hack 39: Copy a link query from a third party link and add it to links on your site

---
+++ webflow-hacks/misc-hacks/39/39.ts
@@ -0,0 +1,26 @@
+// when DOM is ready, run the function
+document.addEventListener('DOMContentLoaded', function () {
+  // if the page url lacks a query string
+  if (!window.location.search) return;
+  // get all links with href attribute
+  const allLinks = document.querySelectorAll('a[href]');
+  allLinks.forEach((link) => {
+    // get the link's href attribute value
+    const hrefVal = link.getAttribute('href');
+    if (!hrefVal || hrefVal.startsWith('#')) return;
+    let href;
+    // if the link's href attribute already has its own query string
+    if (hrefVal.includes('?')) {
+      // append the page url's query string
+      href = hrefVal + '&' + window.location.search.split('?')[1];
+    } else {
+      href = hrefVal + window.location.search;
+    }
+    link.setAttribute('href', href);
+  });
+});
+
+// uses HTML5 history API to manipulate the location bar
+setTimeout(() => {
+  history.replaceState('', document.title, window.location.origin + window.location.pathname);
+});
