19d8f85a9643e98a0230eb659a7163cd575ef784
Brian Musuku <brayanfinsweet@github.com>
2022-06-17 13:43:06 +0300

    Hack 37: Open the last tab the user had open when they revisit the page

---
+++ webflow-hacks/misc-hacks/37/37.ts
@@ -0,0 +1,27 @@
+// when DOM ready
+document.addEventListener('DOMContentLoaded', function () {
+  const TAB_BUTTON_SELECTOR = '.tab-button';
+  const tabButtons = document.querySelectorAll<HTMLAnchorElement>(TAB_BUTTON_SELECTOR);
+  // early return if no tab buttons found
+  if (tabButtons.length === 0) return;
+  if (localStorage.getItem('tab')) {
+    const currentIndex = Number(localStorage.getItem('tab'));
+    // select the current tab
+    const targetNode = tabButtons[currentIndex];
+    console.log(targetNode);
+    triggerMouseEvent(targetNode, 'mouseup');
+    triggerMouseEvent(targetNode, 'click');
+    targetNode.focus();
+  }
+  tabButtons.forEach(function (tabButton, index) {
+    tabButton.addEventListener('click', function () {
+      // set the 'tab' item in localStorage to the index of the tab button
+      localStorage.setItem('tab', String(index));
+    });
+  });
+});
+
+function triggerMouseEvent(node: EventTarget, eventType: string) {
+  const clickEvent = new Event(eventType, { bubbles: true });
+  node.dispatchEvent(clickEvent);
+}
