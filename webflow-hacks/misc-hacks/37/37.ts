7f2d5ba8af8808f72b7158453932fdafd8fa3d35
Brian Musuku <brayanfinsweet@github.com>
2022-06-23 14:33:35 +0300

    feat: use attributes to choose default tab instead of buggy click()

--- webflow-hacks/misc-hacks/37/37.ts
+++ webflow-hacks/misc-hacks/37/37.ts
@@ -1,27 +1,19 @@
-// when DOM ready
 document.addEventListener('DOMContentLoaded', function () {
-  const TAB_BUTTON_SELECTOR = '.tab-button';
-  const tabButtons = document.querySelectorAll<HTMLAnchorElement>(TAB_BUTTON_SELECTOR);
-  // early return if no tab buttons found
+  const tabButtons = document.querySelectorAll<HTMLAnchorElement>('.w-tab-link');
   if (tabButtons.length === 0) return;
-  if (localStorage.getItem('tab')) {
-    const currentIndex = Number(localStorage.getItem('tab'));
-    // select the current tab
-    const targetNode = tabButtons[currentIndex];
-    console.log(targetNode);
-    triggerMouseEvent(targetNode, 'mouseup');
-    triggerMouseEvent(targetNode, 'click');
-    targetNode.focus();
-  }
+
+  // add listeners and reset attributes
   tabButtons.forEach(function (tabButton, index) {
+    tabButton.setAttribute('aria-selected', `false`);
+    tabButton.classList.remove('w--current');
     tabButton.addEventListener('click', function () {
-      // set the 'tab' item in localStorage to the index of the tab button
       localStorage.setItem('tab', String(index));
     });
   });
-});
 
-function triggerMouseEvent(node: EventTarget, eventType: string) {
-  const clickEvent = new Event(eventType, { bubbles: true });
-  node.dispatchEvent(clickEvent);
-}
+  // restore previously selected tab
+  const selectedTabIndex = Number(localStorage.getItem('tab')) || 0;
+  const selectedTabButton = tabButtons[selectedTabIndex];
+  selectedTabButton.classList.add('w--current');
+  selectedTabButton.setAttribute('aria-selected', 'true');
+});
