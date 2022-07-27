6ec208d4aeb7b630108a104112a2d6af86f71abc
Brian Musuku <brayanfinsweet@github.com>
2022-07-27 06:45:26 +0300

    refactor: clean code add submission prevention mechanism

--- webflow-hacks/form-hacks/18/18.js
+++ webflow-hacks/form-hacks/18/18.js
@@ -1,40 +1,33 @@
 'use strict';
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
-  // make an array of invalid domains
   const invalidDomains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'competitor.com'];
-  // set the fs-hacks selector
   const FORM_SELECTOR = '[fs-hacks-element="form"]';
   const EMAIL_SELECTOR = '[fs-hacks-element="email"]';
-  // get the dom elements
   const formElement = document.querySelector(FORM_SELECTOR);
   const emailInput = document.querySelector(EMAIL_SELECTOR);
-  // check if the elements exists
   if (!formElement || !emailInput) return;
-  // on submit button click
-  formElement.addEventListener('submit', function () {
-    // validate the email
+  formElement.addEventListener('submit', function (e) {
     if (!validateEmail(emailInput.value)) {
-      // clear the email input
       emailInput.value = '';
-      // set the error message in placeholder
       emailInput.setAttribute('placeholder', 'Please enter a valid email address');
-      // prevent the form from submitting
+      e.stopPropagation();
       return false;
     }
-    // split email at '@' character to get domain
     const domainPart = emailInput.value.split('@')[1];
-    // if the domain exists in the invalidDomains array
     if (invalidDomains.includes(domainPart)) {
-      // clear email field
       emailInput.value = '';
-      // add a 'use business mail' placeholder
       emailInput.setAttribute('placeholder', 'Please enter a business email');
-      // prevent form submission
+      e.stopPropagation();
       return false;
     }
   });
 });
+/**
+ * Validates an email address
+ * @param email email address to validate
+ * @returns boolean true if valid, false if not
+ */
 const validateEmail = (email) => {
   const emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
