44e791106a892d65ceb0037527a367ad1d494030
Brian Musuku <brayanfinsweet@github.com>
2022-08-17 00:26:16 +0300

    refactor: remove comment

--- webflow-hacks/misc-hacks/13/13.js
+++ webflow-hacks/misc-hacks/13/13.js
@@ -3,26 +3,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const BACK_ONE_SELECTOR = '[fs-hacks-element="hack13-back-1"]';
   const BACK_TWO_SELECTOR = '[fs-hacks-element="hack13-back-2"]';
-  // on click
   const backOne = document.querySelector(BACK_ONE_SELECTOR);
-  backOne === null || backOne === void 0
-    ? void 0
-    : backOne.addEventListener('click', function (e) {
-        // prevent default behavior
-        e.preventDefault();
-        // Go back 1 page
-        window.history.back();
-        // can also use
-        // window.history.go(-1);
-      });
-  // on click
   const backTwo = document.querySelector(BACK_TWO_SELECTOR);
-  backTwo === null || backTwo === void 0
-    ? void 0
-    : backTwo.addEventListener('click', function (e) {
-        // prevent default behavior
-        e.preventDefault();
-        // Go back 2 pages
-        window.history.go(-2);
-      });
+  if (!backOne || !backTwo) return;
+  backOne.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.history.back();
+  });
+  backTwo.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.history.go(-2);
+  });
 });
--- webflow-hacks/misc-hacks/13/13.ts
+++ webflow-hacks/misc-hacks/13/13.ts
@@ -2,25 +2,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const BACK_ONE_SELECTOR = '[fs-hacks-element="hack13-back-1"]';
   const BACK_TWO_SELECTOR = '[fs-hacks-element="hack13-back-2"]';
-
-  // on click
   const backOne = document.querySelector<HTMLDivElement>(BACK_ONE_SELECTOR);
   const backTwo = document.querySelector<HTMLDivElement>(BACK_TWO_SELECTOR);
   if (!backOne || !backTwo) return;
+
   backOne.addEventListener('click', function (e) {
-    // prevent default behavior
     e.preventDefault();
-    // Go back 1 page
     window.history.back();
-    // can also use
-    // window.history.go(-1);
   });
 
-  // on click
   backTwo.addEventListener('click', function (e) {
-    // prevent default behavior
     e.preventDefault();
-    // Go back 2 pages
     window.history.go(-2);
   });
 });
