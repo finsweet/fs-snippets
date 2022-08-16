d03ded567a32a4a92ab3076cd2a7873808f48fdd
Brian Musuku <brayanfinsweet@github.com>
2022-08-17 00:57:35 +0300

    refactor: remove redudant comments

--- webflow-hacks/form-hacks/1.js
+++ webflow-hacks/form-hacks/1.js
@@ -1,42 +1,22 @@
 'use strict';
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
-  // declare selectors
   const SELECT_FIELD_SELECTOR = '[fs-hacks-element="interest-select-field"]';
   const EMAIL_SELECTOR = '[fs-hacks-element="email-form"]';
   const SUCCESS_TEXT_SELECTOR = '[fs-hacks-element="success-text"]';
-  // Get the select field.
   const interestSelectField = document.querySelector(SELECT_FIELD_SELECTOR);
-  // get the email-Form element.
   const emailForm = document.querySelector(EMAIL_SELECTOR);
-  // get the success-text element.
   const successText = document.querySelector(SUCCESS_TEXT_SELECTOR);
-  if (!interestSelectField || !emailForm || !successText) {
-    return;
-  }
-  // declare & initialize the customSuccessMessage variable
-  // with the value of the option selected
-  // on the interest select field
+  if (!interestSelectField || !emailForm || !successText) return;
   let customSuccessMessage = interestSelectField.value;
-  // every time the option on the select field changes
   interestSelectField.addEventListener('change', () => {
-    // assign the new selected option's value to the customSuccessMessage variable
     customSuccessMessage = interestSelectField.value;
   });
-  // when the form's submit button is clicked
+  // change the success message when the form is submitted
   emailForm.addEventListener('submit', (event) => {
-    // if the user selected an option on the select field
-    if (customSuccessMessage) {
-      // change text of the success-text field
-      successText.innerText = `Thank you! We'll focus on ${customSuccessMessage} for future F'in sweet Webflow Hacks!`;
-      // then submit the form
-      return true;
-    }
+    if (!customSuccessMessage) return;
+    successText.innerText = `Thank you! We'll focus on ${customSuccessMessage} for future F'in sweet Webflow Hacks!`;
     event.stopPropagation();
-    // prevent the form from submitting
-    // focus on the select field
     interestSelectField.focus();
-    // stop form submission
-    return false;
   });
 });
