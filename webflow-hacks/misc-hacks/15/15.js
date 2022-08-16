240c470148096fc6ee78319fbfe320718f71789e
Brian Musuku <brayanfinsweet@github.com>
2022-08-17 00:05:17 +0300

    refactor: remove redudant comments

--- webflow-hacks/misc-hacks/15/15.js
+++ webflow-hacks/misc-hacks/15/15.js
@@ -1,24 +1,15 @@
 'use strict';
-// when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
   const MENU_BUTTON_SELECTOR = '[fs-hacks-element="menu-button"]';
-  // get the anchor link buttons
   const menuBtns = document.querySelectorAll(MENU_BUTTON_SELECTOR);
-  if (!menuBtns) return;
-  // for each menu button
   menuBtns.forEach(function (menuBtn) {
-    // when each button is clicked
     menuBtn.addEventListener('click', function () {
-      // set a short timeout before taking action
-      // so as to allow hash to be set
       setTimeout(removeHash);
     });
   });
 });
 /**
- * This function uses HTML5 history API to manipulate the location bar
- * to remove the hash from the URL.
- * @returns void
+ * Remove the hash from the URL.
  */
 function removeHash() {
   history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
--- webflow-hacks/misc-hacks/15/15.ts
+++ webflow-hacks/misc-hacks/15/15.ts
@@ -1,22 +1,16 @@
-// when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
   const MENU_BUTTON_SELECTOR = '[fs-hacks-element="menu-button"]';
-  // get the anchor link buttons
   const menuBtns = document.querySelectorAll(MENU_BUTTON_SELECTOR);
-  // for each menu button
+
   menuBtns.forEach(function (menuBtn) {
-    // when each button is clicked
     menuBtn.addEventListener('click', function () {
-      // set a short timeout before taking action
-      // so as to allow hash to be set
       setTimeout(removeHash);
     });
   });
 });
 
 /**
- * This function uses HTML5 history API to manipulate the location bar
- * to remove the hash from the URL.
+ * Remove the hash from the URL.
  */
 function removeHash() {
   history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
