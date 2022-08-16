895316f06b346c4a1fcf80250a4edb2adf7970dd
Brian Musuku <brayanfinsweet@github.com>
2022-08-17 00:32:57 +0300

    refactor: remove comments

--- webflow-hacks/form-hacks/11/11.js
+++ webflow-hacks/form-hacks/11/11.js
@@ -1,19 +1,12 @@
 'use strict';
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
-  // selector constants
   const SHOW_PAGE_URL_SELECTOR = '[fs-hacks-element="show-page-url"]';
   const PAGE_URL_INPUT_SELECTOR = '[fs-hacks-element="page-url-input"]';
-  // get elements
-  const page_url = document.querySelector(SHOW_PAGE_URL_SELECTOR);
-  const page_url_input = document.querySelector(PAGE_URL_INPUT_SELECTOR);
-  // early return if the element is not found
-  if (!page_url || !page_url_input) return;
-  // get & store the page url in a variable
+  const pageUrl = document.querySelector(SHOW_PAGE_URL_SELECTOR);
+  const pageUrlInput = document.querySelector(PAGE_URL_INPUT_SELECTOR);
+  if (!pageUrl || !pageUrlInput) return;
   const url = location.href;
-  // set the page url as the hidden input's value
-  page_url_input.value = url;
-  // show the page url as text on the page
-  // this last line is only used as a visual for our Hack!
-  page_url.innerText = url;
+  pageUrlInput.value = url;
+  pageUrl.innerText = url;
 });
--- webflow-hacks/form-hacks/11/11.ts
+++ webflow-hacks/form-hacks/11/11.ts
@@ -1,18 +1,12 @@
 // when the DOM is ready
 document.addEventListener('DOMContentLoaded', function () {
-  // selector constants
   const SHOW_PAGE_URL_SELECTOR = '[fs-hacks-element="show-page-url"]';
   const PAGE_URL_INPUT_SELECTOR = '[fs-hacks-element="page-url-input"]';
-  // get elements
   const pageUrl = document.querySelector<HTMLDivElement>(SHOW_PAGE_URL_SELECTOR);
   const pageUrlInput = document.querySelector<HTMLInputElement>(PAGE_URL_INPUT_SELECTOR);
-  // early return if the element is not found
   if (!pageUrl || !pageUrlInput) return;
-  // get & store the page url in a variable
+
   const url = location.href;
-  // set the page url as the hidden input's value
   pageUrlInput.value = url;
-  // show the page url as text on the page
-  // this last line is only used as a visual for our Hack!
   pageUrl.innerText = url;
 });
