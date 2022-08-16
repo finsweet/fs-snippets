fcd28e6f51965e3a3b413029ab22cfc8f4ff7d0f
Brian Musuku <brayanfinsweet@github.com>
2022-08-17 00:29:22 +0300

    refactor: remove redudant comments

--- webflow-hacks/misc-hacks/12/12.js
+++ webflow-hacks/misc-hacks/12/12.js
@@ -1,18 +1,11 @@
 'use strict';
-// when DOM ready
 document.addEventListener('DOMContentLoaded', function () {
-  // selector constants
   const BUTTON_SELECTOR = '[fs-hacks-element="hack-button"]';
   const TEXT_SELECTOR = '[fs-hacks-element="hack-text"]';
-  // get button and text elements
   const button = document.querySelector(BUTTON_SELECTOR);
   const textElement = document.querySelector(TEXT_SELECTOR);
-  // early return if no button
   if (!button || !textElement) return;
-  // on button click listener
   button.addEventListener('click', () => {
-    // function - start
     textElement.innerHTML = 'This is me on Webflow.';
-    // function - end
   });
 });
--- webflow-hacks/misc-hacks/12/12.ts
+++ webflow-hacks/misc-hacks/12/12.ts
@@ -1,14 +1,10 @@
-// when DOM ready
 document.addEventListener('DOMContentLoaded', function () {
-  // selector constants
   const BUTTON_SELECTOR = '[fs-hacks-element="hack-button"]';
   const TEXT_SELECTOR = '[fs-hacks-element="hack-text"]';
-  // get button and text elements
   const button = document.querySelector(BUTTON_SELECTOR);
   const textElement = document.querySelector(TEXT_SELECTOR);
-  // early return if no button
   if (!button || !textElement) return;
-  // on button click listener
+
   button.addEventListener('click', () => {
     textElement.innerHTML = 'This is me on Webflow.';
   });
