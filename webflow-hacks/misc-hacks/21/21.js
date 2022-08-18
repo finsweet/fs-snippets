428ac8fa5bc0b7725531be40644e2d458f3580d5
Brian Musuku <brayanfinsweet@github.com>
2022-08-18 12:54:59 +0300

    refactor: remove unneccessary comments

--- webflow-hacks/misc-hacks/21/21.js
+++ webflow-hacks/misc-hacks/21/21.js
@@ -1,9 +1,7 @@
 'use strict';
 /* put all of this code in <head> of Site Settings if you want to
 gate every page on your site until the age gate is filled out! */
-// if no validAge storahe *AND* you are not on the Age Gate page (update post video-record)
-// this *AND* is needed if you add this redirect code to site settings. Without the AND you will be caught in an infinite redirect loop!
-const AGE_GATE_URL = 'https://finsweethacks.com/success/21-age-gate';
+const AGE_GATE_URL = '/success/21-age-gate';
 if (!localStorage.getItem('validAge') && window.location.href !== AGE_GATE_URL) {
   // redirect to /age-gate page
   window.location.replace(AGE_GATE_URL);
--- webflow-hacks/misc-hacks/21/gate.ts
+++ webflow-hacks/misc-hacks/21/gate.ts
@@ -11,26 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const errorDiv = document.querySelector<HTMLDivElement>(ERROR_DIV_SELECTOR);
   const PROTECTED_PAGE = 'https://finsweethacks.com/21';
   if (!form || !yearElement || !monthElement || !dayElement || !errorDiv) return;
-  // on form submit.
+
   form.addEventListener('submit', () => {
-    // get year, month & day values
     const year = yearElement.value;
     const month = monthElement.value;
     const day = dayElement.value;
-    // make date string
-    // e.g. '2000-09-02'
     const date = year + '/' + month + '/' + day;
-    // if user's age >= 21
+
     if (getAge(date) >= 21) {
-      // set validAge to localStorage
       localStorage.setItem('validAge', 'true');
-      // redirect to hack page
       window.location.replace(PROTECTED_PAGE);
     } else {
-      // if user's age < 21
-      // show error message
       errorDiv.style.display = 'block';
-      // remove error message after 2 sec
+
       setTimeout(() => {
         errorDiv.style.display = 'none';
       }, 2000);
@@ -39,27 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /**
- * This function returns the age of the user in years. It takes a date string as an argument.
+ * Returns the age of the user in years.
  * @param {string} dateString - A date string in the format 'YYYY-MM-DD'.
  * @returns {number} - The age of the user in years.
- * */
+ **/
 function getAge(dateString: string): number {
-  // get today's date
   const today = new Date();
-  // get user birth date
   const birthDate = new Date(dateString);
-  // calculate user age
-  // subtract their birth year from current year
   let age = today.getFullYear() - birthDate.getFullYear();
-  // // subtract user birth month from current month
   const month = today.getMonth() - birthDate.getMonth();
-  // if month <= 0
-  // OR
-  // today's date is less than user's birth date
+
+  // if today's date is less than user's birth date
   if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-    // reduce user's age by 1
     age -= 1;
   }
-  // return user's current age
+
   return age;
 }
